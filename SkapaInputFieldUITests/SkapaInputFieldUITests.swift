9e1815fa375d25a57d6586fd0c1858522ececa19
Cem Lapovski <therealcemski@gmail.com>
2023-06-20 16:33:15 +0200

    UI tests

--- SkapaInputFieldUITests/SkapaInputFieldUITests.swift
+++ SkapaInputFieldUITests/SkapaInputFieldUITests.swift
@@ -9,25 +9,86 @@ import XCTest
 
 final class SkapaInputFieldUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    var app: XCUIApplication!
+    
+    override func setUp() {
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    override func tearDown() {
+        app = nil
+    }
+    
+    /// Test if the app toggles the visibility for the InputField
+    ///
+    func testInputFieldVisibilityButton() {
+        let visibilityButton = app.buttons["visibilityButton"]
+        let usernameTextfield = app.textFields["username"]
+        let passwordSecureTextfield = app.secureTextFields["password"]
+        let passwordTextfield = app.textFields["visiblePassword"]
+        
+        XCTAssertTrue(visibilityButton.exists)
+        XCTAssertTrue(usernameTextfield.exists)
+        XCTAssertTrue(passwordSecureTextfield.exists)
+        XCTAssertFalse(passwordTextfield.exists)
+        
+        
+        visibilityButton.tap()
+        
+        XCTAssertTrue(passwordTextfield.exists)
+        XCTAssertFalse(passwordSecureTextfield.exists)
+        
+        XCTAssertFalse(app.secureTextFields.count == 1)
+        XCTAssertFalse(app.textFields.count == 1)
+        
+        XCTAssertTrue(app.secureTextFields.count == 0)
+        XCTAssertTrue(app.textFields.count == 2)
+        
+        
+    }
+    
+    func testInputFieldCharacterCount() {
+        let passwordSecureField = app.secureTextFields["password"]
+        let counter = app.staticTexts["counter"]
+        
+        passwordSecureField.tap()
+        passwordSecureField.typeText("1234567")
+        XCTAssertTrue(counter.exists)
+        XCTAssertEqual("7/8", counter.label)
+        
+        passwordSecureField.typeText("89")
+        
+        XCTAssertFalse(counter.exists)
+    }
+    
+    func testHintMessageStates() {
+        let passwordSecureTextField = app.secureTextFields["password"]
+        let hintStaticText = app.staticTexts["Only numeric input allowed"]
+        let successStaticText = app.staticTexts["Success"]
+        let errorStaticText = app.staticTexts["Please only enter up to 8 characters"]
+        
+        XCTAssertTrue(hintStaticText.exists)
+        XCTAssertEqual(hintStaticText.label, "Only numeric input allowed")
+        XCTAssertFalse(successStaticText.exists)
+        XCTAssertFalse(errorStaticText.exists)
+        
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("12345678")
+        
+        XCTAssertFalse(hintStaticText.exists)
+        XCTAssertTrue(successStaticText.exists)
+        XCTAssertEqual(successStaticText.label, "Success")
+        XCTAssertFalse(errorStaticText.exists)
+        
+        passwordSecureTextField.typeText("123456789")
+        
+        XCTAssertFalse(hintStaticText.exists)
+        XCTAssertFalse(successStaticText.exists)
+        XCTAssertTrue(errorStaticText.exists)
+        XCTAssertEqual(errorStaticText.label, "Please only enter up to 8 characters")
+        
     }
 
     func testLaunchPerformance() throws {
